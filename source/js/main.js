79b3e0b48f3ebec8238e018d460c7a8c95ffe869
Murad <abishlimurad@gmail.com>
2023-09-30 06:41:33 +0300

    70%

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,20 @@
 const body = document.body;
 const footerBody = document.querySelector('.footer-body');
 const footerHideButton = document.querySelector('.footer-hide');
+const slider = document.querySelector('.slider-container');
+const images = document.querySelectorAll('.slider-images__item');
+
 footerBody.style.setProperty('--footer-body-height', footerBody.scrollHeight + 'px');
+
+const sliderImageHandler = () => {
+  footerHideButton.classList.contains('footer-hide--active') ? images.forEach(image => image.classList.add('slider-images__item--large')) : images.forEach(image => image.classList.remove('slider-images__item--large'));
+}
 
 const hideFooterHandler = () => {
   footerHideButton.classList.toggle('footer-hide--active');
   footerHideButton.classList.contains('footer-hide--active') ? footerBody.style.maxHeight = 0 : footerBody.style.maxHeight = '';
+
+  sliderImageHandler();
 }
 
 footerHideButton.addEventListener('click', hideFooterHandler);
@@ -25,18 +34,13 @@ var swiper = new Swiper('.slider-container', {
   }
 });
 
-const slider = document.querySelector('.slider-container');
-const images = document.querySelectorAll('.slider-images__item');
 swiper.on('slideChange', function () {
-    const activeSlide = slider.querySelector('.swiper-slide-active');
-    const slideOrder = activeSlide.getAttribute('data-order');
-    images.forEach((image) => {
-        const imageOrder = image.getAttribute('data-order');
-        if (imageOrder === slideOrder) {
-            image.classList.add('slider-images__item--active');
-        } else {
-            image.classList.remove('slider-images__item--active');
-        }
-
-    });
+  const activeIndex = swiper.realIndex; // Получаем индекс текущего активного слайда
+  images.forEach((image, index) => {
+      if (index === activeIndex) {
+          image.classList.add('slider-images__item--active');
+      } else {
+          image.classList.remove('slider-images__item--active');
+      }
+  });
 });
