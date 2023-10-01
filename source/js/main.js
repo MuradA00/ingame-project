4a4d69c0b53bad012ad7e4f9a6c43b6be8a35433
Murad <abishlimurad@gmail.com>
2023-10-01 14:06:57 +0300

    100%

--- source/js/main.js
+++ source/js/main.js
@@ -4,21 +4,9 @@ const footerHideButton = document.querySelector('.footer-hide');
 const slider = document.querySelector('.slider-container');
 const images = document.querySelectorAll('.slider-images__item');
 const sliderInfoBlocks = document.querySelectorAll('.slider-info');
+const headerBurger = document.querySelector('.header-burger');
+const menu = document.querySelector('.nav');
 
-footerBody.style.setProperty('--footer-body-height', footerBody.scrollHeight + 'px');
-
-const sliderImageHandler = () => {
-  footerHideButton.classList.contains('footer-hide--active') ? images.forEach(image => image.classList.add('slider-images__item--large')) : images.forEach(image => image.classList.remove('slider-images__item--large'));
-}
-
-const hideFooterHandler = () => {
-  footerHideButton.classList.toggle('footer-hide--active');
-  footerHideButton.classList.contains('footer-hide--active') ? footerBody.style.maxHeight = 0 : footerBody.style.maxHeight = '';
-
-  sliderImageHandler();
-}
-
-footerHideButton.addEventListener('click', hideFooterHandler);
 const slidesNum = 9;
 var swiper = new Swiper('.slider-container', {
 freeMode: false,
@@ -29,7 +17,10 @@ loopAdditionalSlides: slidesNum,
 loopedSlides: slidesNum,
 slidesPerGroup: 1,
 slidesPerView: 'auto',
-speed: 700,
+speed: 300,
+autoplay: {
+  delay: 5000
+},
 breakpoints: {
   576: {
     spaceBetween: 40
@@ -54,3 +45,32 @@ swiper.on('slideChange', function () {
     }
   });
 });
+
+
+footerBody.style.setProperty('--footer-body-height', footerBody.scrollHeight + 'px');
+
+const sliderImageHandler = () => {
+  footerHideButton.classList.contains('footer-hide--active') ? images.forEach(image => image.classList.add('slider-images__item--large')) : images.forEach(image => image.classList.remove('slider-images__item--large'));
+}
+const menuHandler = () => {
+  headerBurger.classList.toggle('header-burger--active');
+
+  if (headerBurger.classList.contains('header-burger--active')) {
+    menu.classList.add('nav--active')
+    document.documentElement.style.overflow = 'hidden';
+    body.style.overflow = 'hidden'
+  } else {
+    menu.classList.remove('nav--active')
+    document.documentElement.style.overflow = '';
+    body.style.overflow = ''
+  }
+}
+const hideFooterHandler = () => {
+  footerHideButton.classList.toggle('footer-hide--active');
+  footerHideButton.classList.contains('footer-hide--active') ? footerBody.style.maxHeight = 0 : footerBody.style.maxHeight = '';
+
+  sliderImageHandler();
+}
+
+footerHideButton.addEventListener('click', hideFooterHandler);
+headerBurger.addEventListener('click', menuHandler);
